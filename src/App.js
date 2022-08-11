babbc36155246e8a64339671a0a9c1a97f7e5bc0
Facutota <medinafacundom@gmail.com>
2022-08-11 10:32:45 -0300

    Primer subida

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
 import './App.css';
+import ExampleRefs from './components/ExampleRefs'
+import ExampleInputForwardRef from './components/ExampleForwardRef';
+import { Slider, Slide } from './components/Slider';
 
 function App() {
+  const [name, setName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const nameInputRef = useRef(null)
+  const sliderRef = useRef(null)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    nameInputRef.current.focus()
+    console.log('on submit', name, lastName)
+  }
+
+  const handleButtonClick = (e) => {
+    // e.stopPropagation()
+    e.preventDefault()
+
+    console.log('Hello guys!')
+  }
+
+  const handleSlider = (direction) => {
+    console.log(direction)
+    if (direction === 'next') {
+      sliderRef.current.next()
+    } else {
+      sliderRef.current.prev()
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <ExampleInputForwardRef
+          ref={nameInputRef}
+          label="Nombre"
+          id="name"
+          value={name}
+          name="name"
+          onChange={(e) => setName(e.target.value)} />
+        <ExampleInputForwardRef
+          label="Apellido"
+          id="last_name"
+          value={lastName}
+          name="last_name"
+          onChange={(e) => setLastName(e.target.value)} />
+        <button type="button" onClick={onSubmit}>Enviar</button>
+        <button onClick={(e) => handleButtonClick(e)}>Quiero tener suerte</button>
+      </form>
+
+      <ExampleRefs />
+      <div style={{ marginTop: 50 }}>
+        <Slider ref={sliderRef} options={{
+          slides: {
+            perView: 1,
+          },
+        }}>
+          <Slide>1</Slide>
+          <Slide>2</Slide>
+          <Slide>3</Slide>
+        </Slider>
+      </div>
+      <button onClick={() => handleSlider('prev')}>Anterior</button>
+      <button onClick={() => handleSlider('next')}>Siguiente</button>
     </div>
   );
 }
